a53ff6defe61e5421cb5ae592c51dbf3e83e5808
EmiKusaba <eeemi615@gmail.com>
2019-12-11 17:50:31 -0600

    init file

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-require('dotenv').config()
+//require('dotenv').config()
+
+const getPosts = () => {
+  fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+  .then(res => {
+    if(!res.ok) {
+      throw Error(res.statusText)
+    } return res.json()
+  })
+  .then(posts => arrayOfPosts = posts)
+  .catch(err => console.log(`Error,  ${err}`))
+}
+
+getPosts();
