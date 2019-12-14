66fb5d6d88adbee3b40ef70f6b78c503fc712c4b
EmiKusaba <eeemi615@gmail.com>
2019-12-13 19:24:07 -0600

    added arena

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let arrayOfPokemon = [];
-let arenaOfPokemon = [];
+let leftPokemon = null;
+let rightPokemon = null;
+
 window.onload = function () {
-  getPosts();
-  console.log(arrayOfPokemon)
-  // displayPokemon()
-}
-const getPosts = () => {
+  getAllPokemon();
+};
+
+// Get list of all pokemon
+const getAllPokemon = () => {
   fetch('https://pokeapi.co/api/v2/pokemon/')
     .then(res => {
       if (!res.ok) {
@@ -17,11 +19,12 @@ const getPosts = () => {
       getPokemonData(pokemon);
       arrayOfPokemon.push(pokemon)
     }))
-    .catch(err => console.log(`Error,  ${err}`))
-}
+    .catch(err => console.log(`Error,  ${err}`));
+};
 
+// Get more data about each pokemon
 const getPokemonData = (pokemonObj) => {
-  fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonObj.name + '/')
+  fetch(pokemonObj.url)
     .then(res => {
       if (!res.ok) {
         throw Error(res.statusText)
@@ -32,50 +35,62 @@ const getPokemonData = (pokemonObj) => {
       Object.assign(pokemonObj, data);
     })
     .catch(err => console.log(`Error,  ${err}`));
-}
+};
 
 //display pokemon
-const displayPokemon = (dir) => {
-  const pokemonList = document.getElementById(`${dir}_pokemon`);
+const displayPokemon = (player) => {
+  const pokemonList = document.getElementById(`${player}_pokemon`);
 
-  arrayOfPokemon.map(pokemon => {
-    const li = document.createElement('li')
-    const picture = document.createElement('img')
-    const button = document.createElement('button')
-    button.innerText = "make player"
-    button.addEventListener("click", function () {
-      makeplayer(pokemon.name)
-    })
+  arrayOfPokemon.map(pokemonObj => {
+    const div = document.createElement('div');
+    const hdr = document.createElement('span');
+    const img = document.createElement('img');
+    pokemonList.append(div);
+    div.appendChild(hdr);
+    div.appendChild(img);
 
-    // const info = document.createElement('p')
-    // info.innerText = ""
-    // const infoText = `DOB: ${user.dob.date} STATE: ${user.location.state}`
-    picture.setAttribute('src', `${pokemon.sprites.front_default}`)
-    const text = document.createTextNode(`${pokemon.name}`)
-    li.appendChild(text)
-    li.appendChild(picture)
-    li.appendChild(button)
-    // leftPokemon.append(li)
-    pokemonList.append(li)
+    // To change Pokemon name's first letter as uppercase and rest of letters lowercase
+    let pokemonName = pokemonObj.name;
+    pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
+    hdr.innerText = pokemonName;
 
-  })
-}
+    //when I click the charactor, callback
+    img.setAttribute('src', `${pokemonObj.sprites.front_default}`);
+    img.addEventListener('click', () => {
+      makePlayer(player, pokemonObj);
+      pokemonList.innerHTML = '';
+
+      //when both player select the pokemon, show battle button. 
+      if(leftPokemon && rightPokemon) {
+        const battle = document.createElement('h3');
+        const arenaDiv = document.getElementById('arena_battle');
+        arenaDiv.appendChild(battle);
+        battle.innerText = 'BATTLE!';
+      }
+    });
+  });
+};
 
 //Show the selection
-const makeplayer = (id) => {
-  const battle = document.getElementById("battle");
-  const findPlayer = arrayOfPokemon.find(pokemon => pokemon.name == id)
-  console.log(findPlayer)
+const makePlayer = (player, pokemonObj) => {
+  const playerArenaDiv = document.getElementById(`${player}_arena_pokemon`);
+  const img = document.createElement('img');
+  img.setAttribute('src', `${pokemonObj.sprites.front_default}`);
 
-  arrayOfPokemon.push(arenaOfPokemon)
-  const li = document.createElement("li")
-  li.appendChild(document.createTextNode(arenaOfPokemon))
-  li.style.border = "1px solid red"
-  const picture = document.createElement('img')
-  picture.setAttribute('src', `${pokemon.sprites.front_default}`)
-  li.appendChild(picture)
-  battle.append(li)
-}
+  // Allow changing pokemon
+  if(playerArenaDiv.firstChild) {
+    playerArenaDiv.firstChild.remove();
+  }
+  //add new pokemon
+  playerArenaDiv.appendChild(img);
+
+  //
+  if(player == 'left') {
+    leftPokemon = pokemonObj;
+  } else {
+    rightPokemon = pokemonObj;
+  }
+};
 
 
 //To judge the who's win
