78323678d18c695ae3b00ad41536d718b3133727
EmiKusaba <eeemi615@gmail.com>
2019-12-18 21:14:17 -0600

    added unit test

--- test.js
+++ test.js
@@ -11,65 +11,43 @@ const getPokemonNames = (fetch, offset = 0, num = 20) => {
       }
       return res.json();
     })
-    .then(data => data.results.map(pokemon => {
-      arrayOfPokemon.push(pokemon)
-    }))
-    .catch(err => {
-      console.log(err);
-    });
-};
-
-const getPokemonData = (fetch, pokemon) => {
-  fetch(pokemon.url)
-    .then(res => {
-      if (!res.ok) {
-        throw Error(res.statusText);
-      }
-      return res.json();
-    })
     .then(data => {
-      // Copy data about this pokemon to pokemon
-      Object.assign(pokemon, data);
+      data.results.map(pokemon => {
+        arrayOfPokemon.push(pokemon)
+      })
     })
-    .catch(err => console.log(`Error,  ${err}`));
+    .catch(err => {
+      error = err.message;
+    });
 };
 
 // //To judge the who's win
 const battle = (leftPokemon, rightPokemon) => {
   const leftScore = calculateScore(leftPokemon);
   const rightScore = calculateScore(rightPokemon);
-
-const losers = [];
   if (leftScore === rightScore) {
-    // Draw
-    caption.innerText = "It's a draw!";
-    losers.push(left, right);
+    return "draw";
   } else if (leftScore > rightScore) {
-    // Left wins
-    caption.innerText = "Left wins!";
-    losers.push(right);
+    return "left";
   } else {
-    // Right wins
-    caption.innerText = "Right wins!";
-    losers.push(left);
+    return "right";
   }
-  const calculateScore = (pokemon) => {
-    // To keep it simple, we just sum up all stats
-    const total = pokemon.stats.reduce((sum, x) => sum + x.base_stat, 0);
-    return total;
-  };
+};
 
-
-
+const calculateScore = (pokemon) => {
+  // To keep it simple, we just sum up all stats
+  const total = pokemon.stats.reduce((sum, x) => sum + x.base_stat, 0);
+  return total;
+};
 
 //Unit tests
 
 if (typeof describe === 'function') {
   describe('getPokemonNames', function () {
-    it('Basic', () => {
+    it('Correct url', () => {
       const fakeFetch = (url) => {
         assert.equal('https://pokeapi.co/api/v2/pokemon/?offset=30&limit=20/', url);
-        return new Promise(() => {});
+        return new Promise(() => { });
       };
       getPokemonNames(fakeFetch, 30, 20);
     });
@@ -78,37 +56,107 @@ if (typeof describe === 'function') {
       arrayOfPokemon = [];
       error = null;
       const fakeFetch = (url) => {
-        const MAX_POKEMON = 807;
+        const MAX_POKEMON = 151;
         const params = new URLSearchParams(url.split("?")[1].split("/")[0]);
         const offset = params.get("offset") || 0;
         const limit = params.get("limit") || 0;
         // Out of range
         if (offset + limit > MAX_POKEMON) {
           return Promise.resolve({
-            res: {
-              ok: false,
-              statusText: `Out of range: offset=${offset} limit=${limit}`
-            }
+            ok: false,
+            statusText: `Out of range: offset=${offset} limit=${limit}`
           });
-        }    
-        return Promise.resolve({});    
+        }
+        return Promise.resolve({
+          ok: true,
+          json: () => {
+            results: ["Mewtwo"];
+          }
+        });
       };
 
-      getPokemonNames(fakeFetch, 808, 20);
-      assert.equal(0, arrayOfPokemon.length);
-      // assert.equal(`Out of range: offset=${offset} limit=${limit}`, error);
-
-      //know winner
-      if (typeof describe === 'function') {
-        describe('calculateScore', function () {
-      it('should be able to calculate score', () => {
-        const pikacyu = getPokemonData
-        const total = pokemon.stats.reduce((sum, x) => sum + x.base_stat, 0);
-        return total;
-      })
+      getPokemonNames(fakeFetch, 140, 20)
+        .then(() => {
+          assert.equal(0, arrayOfPokemon.length);
+          assert.equal("Out of range: offset=140 limit=20", error);
+        });
     });
 
+    it('Handle data from API', () => {
+      arrayOfPokemon = [];
+      error = null;
+      const fakeFetch = (url) => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({
+            results: ["Articuno", "Moltres", "Zapdos"]
+          })
+        });
+      };
 
+      getPokemonNames(fakeFetch, 10, 3)
+        .then(() => {
+          assert.equal(null, error);
+          assert.equal(3, arrayOfPokemon.length);
+          assert(["Articuno", "Moltres", "Zapdos"].every((x, i) => {
+            return x === arrayOfPokemon[i];
+          }));
+        });
+    });
+
+    const rattata = {
+      name: "Rattata",
+      stats: [
+        {
+          name: "Strength",
+          base_stat: 10
+        },
+        {
+          name: "Speed",
+          base_stat: 20
+        },
+        {
+          name: "Defence",
+          base_stat: 30
+        },
+      ]
+    };
+
+    const mew = {
+      name: "Mew",
+      stats: [
+        {
+          name: "Strength",
+          base_stat: 100
+        },
+        {
+          name: "Speed",
+          base_stat: 200
+        },
+        {
+          name: "Defence",
+          base_stat: 300
+        },
+      ]
+    };
+
+    describe('calculateScore', function () {
+      it("Correct score calculation", () => {
+        assert.equal(60, calculateScore(rattata))
+      });
+    });
+
+    describe('battle', function () {
+      it("Left wins", () => {
+        assert.equal("left", battle(mew, rattata));
+      });
+      it("Right wins", () => {
+        assert.equal("right", battle(rattata, mew));
+      });
+      it("Draw", () => {
+        assert.equal("draw", battle(mew, mew));
+      });
+    });
   });
 }
 
